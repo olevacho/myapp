76fb9aefcfd476fd8c13c5328290b64d1721d645
Olevacho <oleg@gluu.org>
2025-11-30 14:39:26 +0100

    my custom commit 16

--- releases/20251112_01/config/urls.py
+++ releases/20251112_01/config/urls.py
@@ -26,7 +26,7 @@ from django.http import HttpResponse
 from django.urls import path
 
 def index(request):
-    return HttpResponse("Hello from Django 15!")
+    return HttpResponse("Hello from Django 16!")
 
 urlpatterns = [
     path('', index),  # Root path
